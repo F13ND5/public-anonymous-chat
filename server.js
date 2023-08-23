09d830d7c624d61efdf30c41319db00c0c20769b
F13ND5 <112640850+F13ND5@users.noreply.github.com>
2023-08-23 15:05:22 +0100

    Create Project
    - Add package.json
    - Add node_modules
    - Add server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+const APP_ID = '1657589';
+const APP_KEY = '4c10ca6bca108226474e';
+const APP_SECRET = 'c6d11bc58f12333421d6';
+const APP_CLUSTER = 'eu';
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+var pusher = new Pusher({
+    appId: APP_ID,
+    key: APP_KEY,
+    secret: APP_SECRET,
+    cluster: APP_CLUSTER   
+});
+
+app.post('/message', function(req, res) {
+    var message = req.body.message;
+    pusher.trigger('public-chat', 'message-added', {message});
+    res.sendStatus(200);
+});
+
+app.get('/', function(req, res) {
+    res.sendFile('public/index.html', {root: __dirname})
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log(`app listening on port ${port}!`)
+});
